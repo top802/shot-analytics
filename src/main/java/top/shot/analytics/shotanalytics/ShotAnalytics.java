b09e8d958b19d293be3f970aa72a07fa65b2ef38
top802 <tatrun89@gmail.com>
2023-06-20 11:29:19 +0300

    added analytics per day

--- src/main/java/top/shot/analytics/shotanalytics/ShotAnalytics.java
+++ src/main/java/top/shot/analytics/shotanalytics/ShotAnalytics.java
@@ -1,13 +1,17 @@
 package top.shot.analytics.shotanalytics;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.Collection;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
@@ -22,11 +26,13 @@ import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import top.shot.analytics.shotanalytics.db_query.DatabaseQuery;
 import top.shot.analytics.shotanalytics.jdbc_manager.DatabaseConnection;
 import top.shot.analytics.shotanalytics.model_dto.ShellingCard;
 import top.shot.analytics.shotanalytics.model_dto.ShellingCardInTable;
@@ -34,16 +40,17 @@ import top.shot.analytics.shotanalytics.model_dto.ShellingCardInTable;
 
 public class ShotAnalytics extends Application {
 
-  private Button saveButton, updateButton, deleteButton;
+  private Button saveButton, updateButton, deleteButton, analyticsButton;
   private Label datePickerLabel, strafing, numbersCannonades,
           startStrafing, endStrafing, positionLabel,
-          weaponTypeLabel;
-  private DatePicker datePicker;
+          weaponTypeLabel, positionAnalyticsLabel, dateAnalyticsLabel;
+  private DatePicker datePicker, analyticsDate;
   private TextField strafingInput, numbersCannonadesInput,
           startStrafingInput, endStrafingInput, positionInput,
-          weaponTypeInput;
+          weaponTypeInput, analyticsPosition;
   private LineChart<String, Number> lineChart;
   private TableView<ShellingCardInTable> tableView;
+  private TableView<ShellingCard> analyticTable;
   private String startStrafingData;
   private int id, rowIndex;
 
@@ -60,12 +67,68 @@ public class ShotAnalytics extends Application {
 //      set element on the scene
     setScene(primaryStage, connection);
   }
+  private void setLabels() {
+    datePickerLabel = new Label();
+    datePickerLabel.setText("Дата");
+    positionLabel = new Label();
+    positionLabel.setText("Позиція");
+    dateAnalyticsLabel = new Label("Дата");
+    positionAnalyticsLabel = new Label("Позиція");
+    weaponTypeLabel = new Label();
+    weaponTypeLabel.setText("Тип озброєння");
+    strafing = new Label();
+    strafing.setText("Кількість обстрілів:");
+    numbersCannonades = new Label();
+    numbersCannonades.setText("Кількіть прильотів:");
+    startStrafing = new Label();
+    startStrafing.setText("Початок обстрілу:");
+    endStrafing = new Label();
+    endStrafing.setText("Кінець обстрілу:");
+    saveButton = new Button();
+    saveButton.setText("Зберегти");
+    analyticsButton = new Button("аналітика");
+    updateButton = new Button();
+    updateButton.setText("Оновити");
+    deleteButton = new Button();
+    deleteButton.setText("Видалити");
+
+  }
+
+  private void setInputFields() {
+    positionInput = new TextField();
+    weaponTypeInput = new TextField();
+    strafingInput = new TextField();
+    numbersCannonadesInput = new TextField();
+    startStrafingInput = new TextField();
+    endStrafingInput = new TextField();
+    datePicker = new DatePicker();
+    analyticsDate = new DatePicker();
+    analyticsPosition = new TextField();
+  }
 
   private void setupAnalytics(Connection connection) {
     createAnalyticsPerDayForPosition(connection);
   }
 
   private void createAnalyticsPerDayForPosition(Connection connection) {
+    analyticsButton.setOnAction( event -> {
+      String date = analyticsDate.getValue().toString();
+      String position = analyticsPosition.getText();
+      String query = DatabaseQuery.getAnalyticPerDay(date, position);
+      try(Statement statement = connection.createStatement()){
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()){
+          String positionDB = resultSet.getString("position");
+          System.out.println(positionDB);
+        }
+
+
+      } catch (SQLException exception){
+        System.out.println("ERROR");
+        exception.getStackTrace();
+      }
+    });
+
 //    todo create datapicker and textfield and than create query
   }
 
@@ -135,38 +198,7 @@ public class ShotAnalytics extends Application {
 
   }
 
-  private void setLabels() {
-    datePickerLabel = new Label();
-    datePickerLabel.setText("Дата");
-    positionLabel = new Label();
-    positionLabel.setText("Позиція");
-    weaponTypeLabel = new Label();
-    weaponTypeLabel.setText("Тип озброєння");
-    strafing = new Label();
-    strafing.setText("Кількість обстрілів:");
-    numbersCannonades = new Label();
-    numbersCannonades.setText("Кількіть прильотів:");
-    startStrafing = new Label();
-    startStrafing.setText("Початок обстрілу:");
-    endStrafing = new Label();
-    endStrafing.setText("Кінець обстрілу:");
-    saveButton = new Button();
-    saveButton.setText("Зберегти");
-    updateButton = new Button();
-    updateButton.setText("Оновити");
-    deleteButton = new Button();
-    deleteButton.setText("Видалити");
-  }
 
-  private void setInputFields() {
-    positionInput = new TextField();
-    weaponTypeInput = new TextField();
-    strafingInput = new TextField();
-    numbersCannonadesInput = new TextField();
-    startStrafingInput = new TextField();
-    endStrafingInput = new TextField();
-    datePicker = new DatePicker();
-  }
 
   private void setScene(Stage primaryStage, Connection connection) {
     primaryStage.setTitle("Картка обстрілку");
@@ -178,7 +210,13 @@ public class ShotAnalytics extends Application {
     // create horizontal box for 3 buttons
     HBox buttonsBox = new HBox(20); // 20 - відстань між кнопками
     buttonsBox.getChildren().addAll(saveButton, updateButton, deleteButton);
+//  todo create HBox for analytics elements
+
 //  todo create analytics table
+    analyticTable = new TableView<>();
+    analyticTable.setPadding(new Insets(5,5,5,5));
+    setTableColumnsInAnalyticsTable(analyticTable);
+// todo delete one day
     CategoryAxis xAxis = new CategoryAxis();
     NumberAxis yAxis = new NumberAxis();
     yAxis.setLabel(startStrafingData);
@@ -187,10 +225,17 @@ public class ShotAnalytics extends Application {
 
     VBox inputBox = new VBox(10);
     inputBox.setPadding(new Insets(10,10,10,10));
+//  todo need change UI
+    HBox analyticInputBox = new HBox(5);
+    analyticInputBox.getChildren().addAll(analyticsDate, analyticsPosition, analyticsButton);
 
+    HBox analyticLabelBox = new HBox(5);
+    analyticLabelBox.getChildren().addAll(dateAnalyticsLabel, positionAnalyticsLabel);
+    VBox analyticsBox = new VBox(5);
+    analyticsBox.getChildren().addAll(analyticLabelBox, analyticInputBox);
     inputBox.getChildren().addAll(datePickerLabel, datePicker, positionLabel, positionInput, weaponTypeLabel, weaponTypeInput,
         strafing, strafingInput, numbersCannonades, numbersCannonadesInput, startStrafing, startStrafingInput,
-        endStrafing, endStrafingInput, buttonsBox, lineChart);
+        endStrafing, endStrafingInput, buttonsBox, analyticsBox, analyticTable);
 
     SplitPane sceneElements = new SplitPane();
     sceneElements.setDividerPositions(0.5);
@@ -201,6 +246,8 @@ public class ShotAnalytics extends Application {
     primaryStage.setScene(scene);
     primaryStage.show();
 
+  }
+  private void setTableColumnsInAnalyticsTable(TableView<ShellingCard> analyticTable) {
   }
 
   private void setTableColumns(TableView<ShellingCardInTable> tableView) {
--- src/main/java/top/shot/analytics/shotanalytics/jdbc_manager/DatabaseConnection.java
+++ src/main/java/top/shot/analytics/shotanalytics/jdbc_manager/DatabaseConnection.java
@@ -1,4 +1,4 @@
-package top.shot.analytics.shotanalytics.jpa_manager;
+package top.shot.analytics.shotanalytics.jdbc_manager;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,8 +13,8 @@ public class DatabaseConnection {
 
   public Connection getDatabaseConnection(){
     String databaseName = "shot_analytics";
-    String databaseUserName = "****";
-    String databasePassword = "****";
+    String databaseUserName = "root";
+    String databasePassword = "134679258top";
     String url = "jdbc:mysql://localhost:3306/" + databaseName + "?createDatabaseIfNotExist=true&autoReconnect=true";
     try{
       Class.forName("com.mysql.cj.jdbc.Driver");
